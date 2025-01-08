a7d7d2c99ef2934752922c1db9f1c94439760af7
NITISH <108420904+nitish-arali@users.noreply.github.com>
2025-01-08 10:24:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://skills-tracker-app-frontend.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
